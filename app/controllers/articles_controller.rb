280a7bf91bf8022b5c366a5f2e09ce09b2de3ad0
run4kelly <r2624kel@comcast.net>
2013-03-27 15:27:58 -0600

    add changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,9 @@ class ArticlesController < ApplicationController
   
   def index
     @articles = Article.paginate(:page => params[:page], :per_page => 10)
+    respond_to do |format|
+    format.rss { render :layout => false }
+    end
   end
 
   def show

659a88c65916a8c08e64ca6db81c1ba8b4d95632
rosskinney <r2624kel@comcast.net>
2013-06-24 22:31:42 -0600

    comit config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ WebApp::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
 
   # Specifies the header that your server uses for sending files
@@ -47,6 +47,15 @@ WebApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
